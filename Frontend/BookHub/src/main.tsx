159e3627756efadcff9e051b3a4d0512303a6e69
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-06 06:55:44 +0200

    updates mains imports

--- Frontend/BookHub/src/main.tsx
+++ Frontend/BookHub/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
+import "./styles/variables.css";
 import App from "./App.tsx";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo.ts";
