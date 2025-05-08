d32fdb79cd2619368713008ca46e2f544ff22c94
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-08 06:17:49 +0200

    adds mutations

---
+++ Frontend/BookHub/src/graphql/mutations/signUpMutation.ts
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const SIGNUP_MUTATION = gql`
+  mutation CreateUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      id
+      username
+      email
+    }
+  }
+`;
