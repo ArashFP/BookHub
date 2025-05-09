ea289f4e1e71979214b0fd1747f9398679924eab
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-09 06:37:50 +0200

    adds import, and cleanup

--- Frontend/BookHub/src/App.tsx
+++ Frontend/BookHub/src/App.tsx
@@ -1,12 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return <RouterProvider router={router} />;
 }
 
 export default App;
-
-// const { loading, error, data } = useQuery(HELLO_QUERY);
-// if (loading) return <p>Loading...</p>;
-// if (error) return <p>Error: {error.message}</p>;
