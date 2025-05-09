5e721ea3c1f8f7c0114b197ca53a9706d73b19f6
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-09 05:36:16 +0200

    updates import

--- Frontend/BookHub/src/router.tsx
+++ Frontend/BookHub/src/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About";
 import RootLayout from "./layouts/RootLayout/RootLayout";
-import Shop from "./pages/Shop";
+import Shop from "./pages/Shop/Shop";
 
 export const router = createBrowserRouter([
   {
