4b4cc12bf04f843b1fdad12783b516745ca872c7
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-11 23:52:16 +0200

    updates query to match backend changes to books

--- Frontend/BookHub/src/graphql/queries/bookQueries.ts
+++ Frontend/BookHub/src/graphql/queries/bookQueries.ts
@@ -1,12 +1,15 @@
 import { gql } from "@apollo/client";
 
 export const BOOKS_BY_AUTHOR_QUERY = gql`
-  query BooksByAuthor($author: String!) {
-    booksByAuthor(author: $author) {
+  query BooksByAuthor($author: String, $authorId: ID) {
+    booksByAuthor(author: $author, authorId: $authorId) {
+      id
       title
       description
       publishedYear
       imageURL
+      genre
+      price
     }
   }
 `;
