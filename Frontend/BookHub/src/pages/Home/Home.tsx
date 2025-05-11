679b94a13dcf1f277080e25663e059224fec2995
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-11 23:53:13 +0200

    moves cardgroup from shop to home

--- Frontend/BookHub/src/pages/Home/Home.tsx
+++ Frontend/BookHub/src/pages/Home/Home.tsx
@@ -1,13 +1,33 @@
-import { Text } from "../../components/Text/Text";
+import { useQuery } from "@apollo/client";
 import styles from "./Home.module.css";
+import { BOOKS_BY_AUTHOR_QUERY } from "../../graphql/queries/bookQueries";
+import CardGroup from "../../components/CardGroup/CardGroup";
+import { Book } from "../../graphql/types/Book";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
+  const { data, loading, error } = useQuery(BOOKS_BY_AUTHOR_QUERY, {
+    variables: { author: "J.R.R. Tolkien" },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div className={styles.homeWrapper}>
-      <Text
-        title="Welcome"
-        description="BookHub is your ultimate destination for discovering, exploring, and purchasing books. Whether you're a fan of fiction, non-fiction, or academic literature, BookHub offers a curated collection to suit every reader's taste. Enjoy a seamless shopping experience with personalized recommendations, detailed book descriptions, and secure checkout. Start your reading journey with BookHub today!"
-      />
+      <CardGroup
+        title="Store Favourites!"
+        description="Our shopper have great taste, the epic fantasy trilogy by J.R.R. Tolkien is our most sold series of books"
+      >
+        {data.booksByAuthor.map((book: Book) => (
+          <Card
+            key={book.title}
+            title={book.title}
+            description={book.description}
+            imageUrl={book.imageURL}
+          />
+        ))}
+      </CardGroup>
     </div>
   );
 };
