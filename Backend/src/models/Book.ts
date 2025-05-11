28be4bfac1a83f0eadc23eecafde1fbea3391fe1
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-11 23:50:19 +0200

    adds authorID, price and genre to book interface

--- Backend/src/models/Book.ts
+++ Backend/src/models/Book.ts
@@ -4,17 +4,23 @@ import { IAuthor } from "./Author";
 export interface IBook extends Document {
   title: string;
   author: IAuthor;
+  authorId?: mongoose.Types.ObjectId;
   description?: string;
   publishedYear?: number;
   imageURL?: string;
+  genre?: string;
+  price: number;
 }
 
 const BookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
+  authorId: { type: Schema.Types.ObjectId, ref: "Author" },
   description: String,
   publishedYear: Number,
   imageURL: String,
+  genre: [{ type: String }],
+  price: Number,
 });
 
 export default mongoose.model<IBook>("Book", BookSchema);
