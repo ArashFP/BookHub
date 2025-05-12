2350ba53a0cd04a0a1593bab20a2d0e3744f092e
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-12 04:15:46 +0200

    updates book interface

--- Frontend/BookHub/src/graphql/types/Book.ts
+++ Frontend/BookHub/src/graphql/types/Book.ts
@@ -1,6 +1,10 @@
 export interface Book {
+  id?: string;
   title: string;
+  author: string;
   description?: string;
   publishedYear?: number;
   imageURL?: string;
+  genre?: string[];
+  price?: number;
 }
