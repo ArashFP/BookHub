83499b62098b7f74ebbf77c3487a7b58ad2b2240
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-12 04:16:06 +0200

    adds cartprovider to app

--- Frontend/BookHub/src/App.tsx
+++ Frontend/BookHub/src/App.tsx
@@ -1,9 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <CartProvider>
+      <RouterProvider router={router} />;
+    </CartProvider>
+  );
 }
 
 export default App;
