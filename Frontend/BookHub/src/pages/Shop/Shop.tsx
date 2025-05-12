d070449bfde0133f549ce382df5557238bea2f48
Arash Falah Parsa <arash_afp_94@hotmail.com>
2025-05-12 05:46:19 +0200

    updates to new bookmodal

--- Frontend/BookHub/src/pages/Shop/Shop.tsx
+++ Frontend/BookHub/src/pages/Shop/Shop.tsx
@@ -1,10 +1,10 @@
+import styles from "./Shop.module.css";
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { BookTable } from "../../components/Table/BookTable";
 import { GET_ALL_BOOKS_QUERY } from "../../graphql/queries/bookQueries";
 import { Book } from "../../graphql/types/Book";
-import styles from "./Shop.module.css";
-import { Modal } from "../../components/Modal/Modal";
+import { BookModal } from "../../components/Modal/BookModal/BookModal";
 
 const Shop = () => {
   const [selectedBook, setSelectedBook] = useState<Book | null>(null);
@@ -36,54 +36,11 @@ const Shop = () => {
         )}
       </section>
 
-      <Modal
+      <BookModal
         isOpen={isModalOpen}
         onClose={handleCloseModal}
-        title={selectedBook?.title || "Book Details"}
-      >
-        {selectedBook && (
-          <div className={styles.bookDetailsModal}>
-            <p className={styles.bookDetailRow}>
-              <span className={styles.bookDetailLabel}>Author: </span>
-              <span className={styles.bookDetailValue}>
-                {selectedBook.author}
-              </span>
-            </p>
-            {selectedBook.description && (
-              <p className={styles.bookDetailRow}>
-                <span className={styles.bookDetailLabel}>Description: </span>
-                <span className={styles.bookDetailValue}>
-                  {selectedBook.description}
-                </span>
-              </p>
-            )}
-            {selectedBook.publishedYear && (
-              <p className={styles.bookDetailRow}>
-                <span className={styles.bookDetailLabel}>Published: </span>
-                <span className={styles.bookDetailValue}>
-                  {selectedBook.publishedYear}
-                </span>
-              </p>
-            )}
-            {selectedBook.price && (
-              <p className={styles.bookDetailRow}>
-                <span className={styles.bookDetailLabel}>Price: </span>
-                <span className={styles.bookDetailValue}>
-                  ${selectedBook.price}
-                </span>
-              </p>
-            )}
-            {selectedBook.genre && Array.isArray(selectedBook.genre) && (
-              <p className={styles.bookDetailRow}>
-                <span className={styles.bookDetailLabel}>Genre: </span>
-                <span className={styles.bookDetailValue}>
-                  {selectedBook.genre.join(", ")}
-                </span>
-              </p>
-            )}
-          </div>
-        )}
-      </Modal>
+        book={selectedBook}
+      />
     </main>
   );
 };
